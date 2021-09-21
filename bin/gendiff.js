cc9b7c0ac5999b28f11768cbcc1e5c441237a83f
Alex <box-570@yandex.ru>
2021-09-21 22:51:28 +0300

    add format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,20 +2,11 @@
 import { Command } from 'commander/esm.mjs';
 
 const program = new Command();
+
 program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
+  .option('-f, --format [type]',  'output format')
 
 program.parse(process.argv);
-
-const options = program.opts();
-if (options.version) console.log('1.0.0');
-if (options.help) console.log(`
-  Usage: gendiff [options]
-
-  Compares two configuration files and shows a difference.
-
-  Options:
-    -V, --version        output the version number
-    -h, --help           output usage information
-`)
