92d28e3a2e9f83fecbe5c7524fed7fad2d0a4f3d
Alex <box-570@yandex.ru>
2021-09-21 23:06:03 +0300

    fix format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,6 @@ const program = new Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
   .option('-f, --format [type]',  'output format')
 
 program.parse(process.argv);
