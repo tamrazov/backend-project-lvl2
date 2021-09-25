cb0d2699bdb7159419f2b56a69b65772d9450fca
Alex <box-570@yandex.ru>
2021-09-25 18:31:31 +0300

    fix bugs

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -5,7 +5,7 @@ import genDiff from '../index.js';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('genDiff from fixtures', () => {
-  const output = fs.readFileSync(getFixturePath('expected_file.json'), {encoding: 'utf-8'});
+  const output = fs.readFileSync(getFixturePath('expected_file.json'), { encoding: 'utf-8' });
 
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(output);
 });
