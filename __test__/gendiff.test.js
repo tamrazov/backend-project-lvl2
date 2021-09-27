93fda43edc18a9498fa41622e4c3e54924560df2
Alex <box-570@yandex.ru>
2021-09-27 23:11:40 +0300

    add yml

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -4,8 +4,15 @@ import genDiff from '../index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('genDiff from fixtures', () => {
-  const output = fs.readFileSync(getFixturePath('expected_file.json'), { encoding: 'utf-8' });
+test('genDiff from json', () => {
+  const output = fs.readFileSync(getFixturePath('expected_file.txt'), { encoding: 'utf-8' });
 
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(output);
 });
+
+test('genDiff from yaml', () => {
+  const output = fs.readFileSync(getFixturePath('expected_file.txt'), { encoding: 'utf-8' });
+
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(output);
+});
+
