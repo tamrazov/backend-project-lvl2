d86bd48841ca1538442a11f50c37d6878c76c697
Tamrazov <alexandr.tamrazov@accenture.com>
2021-10-19 23:05:55 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import path from 'path';
 import gendiff from './gendiff.js';
 import parcer from './parcer.js';
+import stylish from './stylish.js';
 
 const main = (path1, path2) => {
   const data1 = parcer(path1, path.extname(path1));
   const data2 = parcer(path2, path.extname(path2));
-  const diff = gendiff(data1, data2)
+  const diff = gendiff(data1, data2);
+  console.log(diff)
+  const result = stylish(diff);
 
-  return gendiff(data1, data2);
+  return result;
 };
 
 export default main;
--- src/stylish.js
+++ src/stylish.js
@@ -2,11 +2,28 @@
 
 
 const stylish = (data) => {
-    const result = '';
+    let result = '{\n';
 
-    
+    for (let item of data) {
+        switch(true) {
+            case item.type === 'deleted':
+                result += `  - ${item.key}: ${item.value}\n`;
+                break;
+            case item.type ===  'unchanged':
+                result += `  ${item.key}: ${item.value}\n`;
+                break;
+            case item.type === 'added':
+                result += `  + ${item.key}: ${item.value}\n`;
+                break;
+            case item.type === 'changed':
+                result += `  +- ${item.key}: ${item.value1}\n`;
+                break;  
+            default:
+                throw new Error(`dont know type ${item.type}`)
+        }
+    }
 
-    return result;
+    return `${result}}`;
 }
 
 export default stylish;
